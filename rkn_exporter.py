034cd66cecef507abb7e37738aa0e615a7fa9b9e
Egor Vasilyev <egor.vasilyev@jetbrains.com>
2022-02-24 20:58:54 +0300

    configured additional args

--- rkn_exporter.py
+++ rkn_exporter.py
@@ -8,7 +8,7 @@ from aiohttp import web
 from concurrent.futures import ThreadPoolExecutor
 from handler import read_file_to_list, \
                     validate_domains, \
-                    return_metrics, initialize_resolver, fill_queue
+                    return_metrics, initialize_resolver, fill_queue, subnets_to_ips
 
 # possibly it's good idea to use contextvars here
 data = 'rkn_is_in_processing 1'
@@ -23,35 +23,46 @@ def parse_args():
     parser = argparse.ArgumentParser(prog='rkn_exporter',
                                      description='''Prometheus exporter developed to indicate what your domains
                                                     are blocked by Roskomnadzor agency to access from Russia''')
+    parser.add_argument('-i', '--ip',
+                        default=os.getenv("APP_IP", '0.0.0.0'),
+                        type=str,
+                        help='IP address (default: 0.0.0.0)')
     parser.add_argument('-p', '--port',
                         default=os.getenv("APP_PORT", 8080),
                         type=int,
                         help='Port to be listened (default: 8080)')
-    parser.add_argument('-t', '--time',
-                        default=os.getenv("APP_CHECK_INTERVAL", 60),
+    parser.add_argument('-c', '--check_interval',
+                        default=os.getenv("APP_CHECK_INTERVAL", 3600),
                         type=int,
                         help='Default time range in seconds to check metrics (default: 60)')
+    parser.add_argument('-d', '--domains',
+                        default=os.getenv("APP_DOMAINS"),
+                        type=str,
+                        help='Path to a file with domains to check. One domain per line (default: No)')
+    parser.add_argument('-s', '--blocked_subnets',
+                        default=os.getenv("APP_SUBNETS"),
+                        type=str,
+                        help='Path to a file with subnets bloked by RKN. One subnet per line (default: No)')
     return parser.parse_args()
 
 
 class Component:
     def __init__(self,
                  name="rkn-exporter",
-                 loop=asyncio.get_event_loop()):
+                 loop=asyncio.get_event_loop(),
+                 args=None):
         self.name = name
         self.loop = loop
+        self.args = args
 
     async def start(self):
-        # Server
-        args = parse_args()
         app = web.Application()
         # For storing global-like variables, feel free to save them in an Application instance
-        app['args'] = args
         runner = web.AppRunner(app)
         app.router.add_get('/metrics', self.handle_work)
 
         await runner.setup()
-        site = web.TCPSite(runner, '0.0.0.0', 8080)
+        site = web.TCPSite(runner, self.args.ip, self.args.port)
         await site.start()
 
     async def handle_work(self, request):
@@ -60,13 +71,18 @@ class Component:
 
 
 class Requestor:
-    def __init__(self, loop=asyncio.get_event_loop()):
+    def __init__(self,
+                 loop=asyncio.get_event_loop(),
+                 args=None):
         self.loop = loop
+        self.args = args
 
     async def handler(self):
         global data
-        domains_set = validate_domains(read_file_to_list('/app/inputs/domains.txt'))
-        blocked_subnets_set = set(read_file_to_list('/app/inputs/blocked_subnets.txt'))
+        domains_set = validate_domains(read_file_to_list(self.args.domains))
+        blocked_subnets_set = set(read_file_to_list(self.args.blocked_subnets))
+        blocked_ips_set = subnets_to_ips(blocked_subnets_set)
+
         resolver = initialize_resolver()
         queue = janus.Queue(maxsize=len(domains_set))
 
@@ -74,15 +90,12 @@ class Requestor:
             # fast but blocking function
             fill_queue(queue=queue, domains_set=domains_set)
 
-            # multithreading almost has no effect because the most heavy
-            # task is checking if ip address is in blocked subnets.
-            # Working how to improve performance...
-            threads_count = 1
+            threads_count = 10
             executor = ThreadPoolExecutor(max_workers=threads_count)
             # you should pass blocking function into executor or start additional
             # event loop inside that function each time it called if you want async behaviour
             futures = [
-                self.loop.run_in_executor(executor, return_metrics, queue, blocked_subnets_set, resolver)
+                self.loop.run_in_executor(executor, return_metrics, queue, blocked_ips_set, resolver)
                 for _ in range(threads_count)
             ]
             raw_data = await asyncio.gather(*futures)
@@ -93,16 +106,18 @@ class Requestor:
             #   Python 3.8, and scheduled for removal in Python 3.10.
             #
             # Read more at: https://stackoverflow.com/a/60315290
-            await asyncio.sleep(3600)
+            await asyncio.sleep(self.args.check_interval)
 
 
 if __name__ == '__main__':
+    args = parse_args()
     loop = asyncio.get_event_loop()
+    print(f'Address: {args.ip}')
 
-    component = Component(loop=loop)
+    component = Component(loop=loop, args=args)
     loop.run_until_complete(component.start())
 
-    requestor = Requestor(loop=loop)
+    requestor = Requestor(loop=loop, args=args)
     loop.run_until_complete(requestor.handler())
 
     loop.run_forever()
